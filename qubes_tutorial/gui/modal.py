7b4470bbd9a51157bfa0a1286b0230d999118f3a
deeplow <deeplower@protonmail.com>
2022-05-29 20:45:14 +0100

    initial refactor to make the modal from file
    Steps are customizable. As such it is important that we can load
    modal screens from a file instead of them being hard-coded

--- qubes_tutorial/gui/modal.py
+++ qubes_tutorial/gui/modal.py
@@ -1,12 +1,31 @@
 import gi
 
 gi.require_version("Gtk", "3.0")
-from gi.repository import Gtk
+from gi.repository import Gtk, GObject
+
+@Gtk.Template(filename='welcome_page.ui')
+class CustomModal(Gtk.Box):
+
+    __gtype_name__ = 'CustomModal'
+
+    button_ok_text = GObject.Property(type=str, default="Next")
+
+    def __init__(self):
+        super().__init__()
 
 
 @Gtk.Template(filename="modal.ui")
 class ModalWindow(Gtk.Window):
-    __gtype_name__ = "modalWindow"
+    __gtype_name__ = "ModalWindow"
+
+    _custom_modal = Gtk.Template.Child()
+
+    def __init__(self):
+        super().__init__()
+
+        self._custom_modal.props.button_ok_text = "Continue »"
+        print("button_ok_text:", self._custom_modal.props.button_ok_text)
+
 
 window = ModalWindow()
 window.show()
