c8bac22d1c66e840889cac3a03c368b56df0b5c8
deeplow <deeplower@protonmail.com>
2022-05-29 20:45:04 +0100

    initial node-transition (string-based)

--- tutorial/tutorial.py
+++ tutorial/tutorial.py
@@ -5,6 +5,7 @@ from queue import Queue
 
 import utils
 import watchers
+from interactions import Interaction
 
 interactions = []
 
@@ -16,20 +17,12 @@ def main():
     parser = argparse.ArgumentParser(
             description='Integrated tutorials tool for Qubes OS')
 
-    action = parser.add_mutually_exclusive_group(required=True)
-
-    action.add_argument('--create',
+    parser.add_argument('--create', '-c',
                         type=argparse.FileType('w', encoding='UTF-8'),
-                        nargs='?',
-                        default=sys.stdout,
+                        metavar="FILE",
                         help='Create a tutorial')
 
-    action.add_argument('--load',
-                        nargs=1,
-                        default='tutorial.yaml',
-                        help='Loading a tutorial from a file')
-
-    parser.add_argument('-s', '--scope',
+    parser.add_argument('--scope', '-s',
                         type=str,
                         help='qubes affected (e.g. --scope=personal,work)')
 
@@ -40,13 +33,32 @@ def main():
         scope = [x.strip() for x in args.scope.split(",")]
 
     interactions_q = Queue()
+    watchers.init_watchers(scope, interactions_q)
 
     if args.create:
         create_tutorial(args.create, scope, interactions_q)
+    else:
+        start_tutorial("tutorial.tut", scope, interactions_q)
 
-def start_tutorial():
+    watchers.stop_watchers(scope)
+
+
+def start_tutorial(infile, scope, interactions_q):
     logging.info("starting tutorial")
-    load_tutorial()
+    tutorial = Tutorial(infile)
+
+    # TODO global logs monitoring
+
+    node = tutorial.get_first_node()
+    while node != "end":
+        logging.info('currently on node "{}"'.format(node))
+        interaction = interactions_q.get(block=True)
+
+        if not tutorial.has_transition(node, interaction):
+            logging.debug("interaction does not transition")
+            continue
+
+        node = tutorial.get_next(node, interaction)
 
 def create_tutorial(outfile, scope, interactions_q):
     logging.info("creating tutorial")
@@ -59,16 +71,28 @@ def create_tutorial(outfile, scope, interactions_q):
 
     except KeyboardInterrupt:
         utils.gen_report(interactions_q)
-    finally:
-        watchers.stop_watchers(scope)
-
-
-def load_tutorial():
-    logging.info("loading tutorial")
 
 def init_gui():
     pass
 
+
+class Tutorial:
+    def __init__(self, infile):
+        pass
+
+    def get_first_node(self):
+        return "start"
+
+    def has_transition(self, node: str, interaction: Interaction) -> bool:
+        return True
+
+    def get_next(self, node: str, interaction: Interaction) -> str:
+        if node == "start":
+            return "node1"
+        if node == "node1":
+            return "node2"
+        if node == "node2":
+            return "end"
 
 if __name__ == "__main__":
     main()
