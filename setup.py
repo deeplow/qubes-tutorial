fc5eaff11c925b3a9bcb2b14c4331bf0f795c3ef
deeplow <deeplower@protonmail.com>
2022-05-29 20:45:15 +0100

    initial RPM packaging

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+''' Setup.py file '''
+import os
+import setuptools
+
+def read_file(file_name):
+    return open(os.path.join(os.path.dirname(__file__), file_name)).read()
+
+setuptools.setup(
+    name='qubes-tutorial',
+    version=read_file('version')[:-1], # remove trailing new line
+    author='deeplow',
+    author_email='deeplower at protonmail.com',
+    description='Qubes Integrated Tutorial Package',
+    license='GPL2+',
+    url='https://github.com/deeplow',
+    keywords='integrated qubes tutorial',
+    long_description=read_file('README.md'),
+    packages=("qubes_tutorial", "qubes_tutorial.gui"),
+    package_data = {
+            'qubes_tutorial.gui': ['*.ui', 'images/*'],
+    },
+    entry_points={
+        'gui_scripts': [
+            'qubes-tutorial = qubes_tutorial.tutorial:main'
+        ]
+    }
+)
